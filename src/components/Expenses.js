99bc93a1d297aee39afc4b2a1ed400bb78233be8
Duy Pham <duypham9895@gmail.com>
2022-09-02 19:29:57 +0700

    Practice the concept of composition

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,14 +2,15 @@ import React from "react";
 
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 const Expenses = ({ expenses }) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {expenses.map(({ id, title, amount, date }) => (
         <ExpenseItem key={id} title={title} amount={amount} date={date} />
       ))}
-    </div>
+    </Card>
   );
 };
 
