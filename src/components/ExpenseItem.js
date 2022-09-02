7172dc7834634279f11a1a2549812c1a5452b5c9
Duy Pham <duypham9895@gmail.com>
2022-09-02 19:00:36 +0700

    Split ExpenseDate to a component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>{date.toLocaleDateString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
